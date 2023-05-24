51f002da38bda90302e0840851c89c07d75737a5
aggelos950 <vassalasagge@yahoo.gr>
2023-05-24 17:12:29 +0300

    form functionality progress

--- src/components/EventFolder/EventModal.jsx
+++ src/components/EventFolder/EventModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 import Image from '../SimpleElementsFolder/Image';
 
@@ -6,12 +6,50 @@ import Image from '../SimpleElementsFolder/Image';
 
 const EventModal = ({ event, onClose }) => {
 
+
+//useStates
+    const [isOpen,setIsOpen] = useState(false);
+    const [isVisible,setIsVisible] = useState(false);
+    const [selected,setSelected] = useState(1);
+    const [inputFields,setInputFields] = useState([
+        {name:'',surname:'',age:''}
+    ])
+
+
+
+
+
+
+//functions
+ 
     function  componentDidMount() {
         document.body.style.overflow = 'hidden';
     }
     
    function  componentWillUnmount() {
         document.body.style.overflow = 'unset';
+    }
+
+   function onChange(event){
+      setSelected(event.target.value);
+      
+      }
+      
+
+    function handleFormChange(index,event){
+        let data = [...inputFields];
+        data[index][event.target.name] = event.target.value;
+        setInputFields(data);
+    }
+
+
+    function handleClick1(event){
+        event.preventDefault();
+        setIsOpen(!isOpen);
+    }
+    function handleClick2(event){
+        event.preventDefault();
+        setIsVisible(!isVisible);
     }
 
     if (event===null){
@@ -21,6 +59,9 @@ const EventModal = ({ event, onClose }) => {
         componentDidMount();
     }
 
+
+
+    //return statement
     return (
         <Modal isOpen={true} onRequestClose={onClose} ariaHideApp={false} style={{
             overlay:{
@@ -48,29 +89,45 @@ const EventModal = ({ event, onClose }) => {
             <hr />
             <form action="">
                 <h1>Book your spot</h1>
+
+
                 <div className='attendats'>
                     <label>Attendats: </label>
-                    <select>
+                    <select onChange={onChange} disabled={isOpen} >
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
                         <option value="4">4</option>
                     </select>
                 </div>
+
+                
                 <div className='typeTicket'>
-                    <input type="radio" value="normal" name="gender" /> Normal
-                    <input type="radio" value="student" name="gender" /> Premnium
-                    <input type="radio" value="unemploye" name="gender" /> VIP
+                    <input type="radio" value="normal" name="gender" disabled={isOpen} /> Normal
+                    <input type="radio" value="student" name="gender" disabled={isOpen} /> Premnium
+                    <input type="radio" value="unemploye" name="gender" disabled={isOpen} /> VIP
                 </div>
-                <button className='formBtn'>Next</button>
-                <div className='info'>
+                <button onClick={handleClick1} className='formBtn'>{isOpen?"Change":"Next"}</button>
+
+
+               
+                {isOpen && <div className='info'>
                 <hr />
-                   <input type='text'placeholder='Name'/>
-                   <input type='text'placeholder='Surname'/>
-                   <input type='text'placeholder='Age'/>
-                   <button className='formBtn'>Next</button>
-                </div>
-                <div className='info'>
+                {inputFields.map((input,index)=>{
+                    return(
+                        <div key={index}>
+                            <input type='text'placeholder='Name' name='name' disabled={isVisible} value={input.name} onChange={event => handleFormChange(index,event)}/>
+                            <input type='text'placeholder='Surname' name='surname' disabled={isVisible} value={input.surname} onChange={event => handleFormChange(index,event)}/>
+                            <input type='text'placeholder='Age' name='age' disabled={isVisible} value={input.age} onChange={event => handleFormChange(index,event)}/>
+                            <br /> <br />
+                        </div>
+                    );
+                })}      
+                   <input type='text' value="20$" disabled={true} className='total'/>
+                   <button onClick={handleClick2} className='formBtn'>{isVisible?"Change":"Next"}</button>
+                </div> }
+
+                {isVisible && <div className='info'>
                 <hr />
                    <input type='text'placeholder='Email'/>
                    <input type='text'placeholder='Card Number'/>
@@ -82,11 +139,12 @@ const EventModal = ({ event, onClose }) => {
                         <option value="Revolut">Revolut</option>
                    </select>
                    <button type='submit' className='formBtn'>Book</button>
-                </div>
+                </div>}
             </form>
+
             <button className='modalBtn' onClick={onClose}>Close</button>
         </Modal>
     )
-}
+            }
 
 export default EventModal;
